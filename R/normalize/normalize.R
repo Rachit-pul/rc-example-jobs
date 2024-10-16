ac98fb9ff9ff3b07789f8b787555dbfe6a3c07bc
Carrie Brown <carrie.brown@psu.edu>
2024-10-16 12:30:12 -0400

    Update R normalize example to provide output and use latest R version

--- R/normalize/normalize.R
+++ R/normalize/normalize.R
@@ -1,17 +1,22 @@
 # read in the biom file
 otu_matrix <- as.matrix(read.table("otu_table.csv.gz"))
+message("data loaded")
 
 # replace any missing values with 0.1
 otu_matrix[is.na(otu_matrix)] <- 0.1
+message("replaced missing values")
 
 # normalize the matrix
 otu_matrix <- scale(otu_matrix)
+message("normalization complete")
 
 # convert all values to the absolute value
 otu_matrix <- apply(otu_matrix, c(1,2), abs)
+message ("absolute value transformation complete")
 
 # transform values using Log2
 otu_matrix <- apply(otu_matrix, c(1,2), log2)
+message("log2 transformation complete")
 
 # write the normalized matrix to normalized_otu_matrix.csv
 write.table(otu_matrix,
